013113c7b460e3c7fbbc0e17b5598fa935ccbb94
Jessie <banderegewigz@gmail.com>
2025-04-22 17:50:35 +0200

    Basic stuff

---
+++ src/test/java/bb/aplikacje/kwiaciarnia/KwiaciarniaApplicationTests.java
@@ -0,0 +1,13 @@
+package bb.aplikacje.kwiaciarnia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KwiaciarniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
